6651d700c9d8582b45bcec4d248adfa9d294beda
Parth <mparth141@gmail.com>
2024-03-09 13:09:09 +0530

    change title of website

--- header.php
+++ header.php
@@ -39,7 +39,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EV Car</title>
+    <title>EV Cars Info: The Ultimate Guide to Price, Features, Performance, News</title>
     <link rel="icon" href="<?= ROOT_URL.'assets/img/favicon.ico'; ?>" type="image/x-icon" />
     <link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/style.css">
     <link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/bootstrap.min.css">
